0b7ad459b960e94dfcf915148f396cc113c75bc2
Viktor Kindrat <kindratvictor07@gmail.com>
2024-11-17 13:09:00 +0200

    Modify with localstorage

--- src/features/cart/cartReducer.js
+++ src/features/cart/cartReducer.js
@@ -2,45 +2,66 @@ const initialState = []
 
 
 function cartReducer(state = initialState, action) {
+    let update = [...state];
     switch (action.type) {
         case "cart/addItem":
             let isExist = state.find(item => item._id === action.payload._id)?._id;
 
             if (isExist) {
-                return state.map(item =>
+                let update = state.map(item =>
                     item._id !== action.payload._id ? item : {
                         ...item,
                         count: item.count + action.payload.count
                     }
                 )
+
+                localStorage.setItem("cart", JSON.stringify(update))
+                return update
             } else {
+                localStorage.setItem("cart", JSON.stringify([...state, action.payload]))
                 return [...state, action.payload]
             }
         case "cart/decrementItem":
             let itemToBeDecremented = state.find(item => item._id === action.payload._id);
 
             if (itemToBeDecremented.count === 1) {
-                return state.filter(item => item._id !== action.payload._id)
+                let update = state.filter(item => item._id !== action.payload._id)
+
+                localStorage.setItem("cart", JSON.stringify(update))
+                return update
             } else {
-                return state.map(item => item._id !== action.payload._id ? item : {
+                let update = state.map(item => item._id !== action.payload._id ? item : {
                     ...item,
                     count: item.count - 1
                 })
+
+                localStorage.setItem("cart", JSON.stringify(update))
+                return update
             }
         case "cart/incrementItem":
-            return state.map(item => item._id !== action.payload._id ? item : {
+            update = state.map(item => item._id !== action.payload._id ? item : {
                 ...item,
                 count: item.count + 1
             })
+
+            localStorage.setItem("cart", JSON.stringify(update))
+            return update
         case "cart/setCount":
-            return state.map(item => item._id !== action.payload._id ? item : {
+            update = state.map(item => item._id !== action.payload._id ? item : {
                 ...item,
                 count: action.payload.count
             })
+
+            localStorage.setItem("cart", JSON.stringify(update))
+            return update
         case "cart/deleteItem":
-            return state.filter(item => item._id !== action.payload._id)
+            update = state.filter(item => item._id !== action.payload._id)
+
+            localStorage.setItem("cart", JSON.stringify(update))
+            return update
         default:
-            return state
+            localStorage.setItem("cart", JSON.stringify(update))
+            return update
     }
 }
 
--- src/features/store.js
+++ src/features/store.js
@@ -1,9 +1,24 @@
 import {configureStore} from '@reduxjs/toolkit'
 import cartReducer from "./cart/cartReducer.js";
 
+const loadStateFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('cart');
+        if (serializedState === null) {
+            return { cart: [] };
+        }
+        return { cart: JSON.parse(serializedState) };
+    } catch (error) {
+        console.error("Could not load state from localStorage", error);
+        return { cart: [] };
+    }
+};
+
+const preloadedState = loadStateFromLocalStorage()
 
 export default configureStore({
     reducer: {
         cart: cartReducer
-    }
+    },
+    preloadedState
 })
